730e2b81cc951546839fa891def99766d2b984d6
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-01-18 19:58:59 +0900

    refactor: remove unused functions

--- src/ToyLisp/Syntax.hs
+++ src/ToyLisp/Syntax.hs
@@ -4,20 +4,15 @@ import qualified Data.Text as T
 
 newtype TextSize = TextSize Int
     deriving (Eq, Ord, Num, Bounded)
+
 instance Show TextSize where
     show (TextSize size) = show size
-
-unTextSize :: TextSize -> Int
-unTextSize (TextSize size) = size
 
 data TextRange = TextRange TextSize TextSize
     deriving (Eq)
 
 instance Show TextRange where
     show (TextRange (TextSize start) (TextSize end)) = show start ++ ".." ++ show end
-
-unTextRange :: TextRange -> (Int, Int)
-unTextRange (TextRange start end) = (unTextSize start, unTextSize end)
 
 newtype Ast = Ast [AstNode]
     deriving (Eq, Show)
