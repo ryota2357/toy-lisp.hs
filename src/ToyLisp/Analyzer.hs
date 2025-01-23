320d60c392a0a0fe4c6ae990b46d3a12a9dab84f
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-01-23 18:11:59 +0900

    feat!: remove analyseAst

--- src/ToyLisp/Analyzer.hs
+++ src/ToyLisp/Analyzer.hs
@@ -1,30 +1,22 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module ToyLisp.Analyzer (analyseAst, expandMacro) where
+module ToyLisp.Analyzer (expandMacro, validateTopLevelNodes) where
 
-import           Data.Maybe      (catMaybes)
-import           ToyLisp.Runtime (MacroInfo (..))
-import           ToyLisp.Syntax  (Ast (..), AstNode (..), SyntaxError (..),
-                                  astNodePosition)
+import qualified Data.Text      as T
+import           ToyLisp.Syntax (Ast (..), AstNode (..), SyntaxError (..),
+                                 astNodePosition)
 
-analyseAst :: Ast -> Either SyntaxError (Ast, [MacroInfo])
-analyseAst (Ast nodes) = do
-    checkTopLevelNodes nodes
-    macros <- collectMacros nodes
-    return (Ast nodes, macros)
-    where
-        checkTopLevelNodes = mapM_ (\case
-            (ListNode _ _) -> Right ()
-            node -> Left $ SyntaxError (astNodePosition node) "Top level expressions must be S-expressions")
-        collectMacros = fmap catMaybes . mapM (\case
-            (ListNode _ nodelist) -> case nodelist of
-                SymbolNode _ "macrodef" : SymbolNode _ name : ListNode _ args : body ->
-                    Right $ Just $ MacroInfo name args (Ast body)
-                SymbolNode pos "defun" : SymbolNode _ "macrodef" : _ ->
-                    Left $ SyntaxError pos "Cannot redefine macrodef"
-                _ -> Right Nothing
-            _ -> Right Nothing)
+data MacroInfo = MacroInfo
+    { macroName :: T.Text
+    , macroArgs :: [AstNode]
+    , macroBody :: Ast
+    } deriving (Show, Eq)
+
+validateTopLevelNodes :: [AstNode] -> Either SyntaxError ()
+validateTopLevelNodes = mapM_ (\case
+    (ListNode _ _) -> Right ()
+    node -> Left $ SyntaxError (astNodePosition node) "Top level expressions must be S-expressions")
 
 expandMacro :: (Ast, [MacroInfo]) -> Either SyntaxError Ast
 expandMacro (Ast _nodes, _infos) =
--- src/ToyLisp/Runtime.hs
+++ src/ToyLisp/Runtime.hs
@@ -3,11 +3,6 @@ module ToyLisp.Runtime where
 import qualified Data.Text      as T
 import           ToyLisp.Syntax
 
-data MacroInfo = MacroInfo
-    { macroName :: T.Text
-    , macroArgs :: [AstNode]
-    , macroBody :: Ast
-    } deriving (Show, Eq)
 
 data LispObject
     = LispInt Integer
