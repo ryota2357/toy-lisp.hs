f6f207f21e260a8c470160fece9587ccd1a36c88
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-01-27 11:11:25 +0900

    feat: first impl of cli app interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,41 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Main where
 
-import           ToyLisp.Evaluator (eval)
-import           ToyLisp.Parser    (parse)
+import qualified Options.Applicative as O
+import qualified ToyLisp             as TL
 
 main :: IO ()
-main = do
-    case parse "(princ (+ 1 2))" of
-        Left err -> print err
-        Right ast -> do
-            !_ <- eval ast
-            putStrLn ""
+main = O.execParser opts >>= runMain
+  where
+    opts = O.info (argumentParser O.<**> O.helper)
+        ( O.fullDesc
+        <> O.header "A simple Lisp interpreter written in Haskell" )
+
+runMain :: Arguments -> IO ()
+runMain args =
+    case args.scriptFilePath of
+        Nothing -> TL.runReplWith config
+        Just path -> do
+            content <- readFile path -- TODO: Handle errors
+            TL.runWith config content
+  where
+    config = TL.RunConfig
+        { TL.runMode = if args.showAst then TL.ShowAstOnly else TL.ExecuteProgram
+        , TL.runPreloads = []
+        }
+
+data Arguments = Arguments
+    { scriptFilePath :: Maybe FilePath
+    , showAst        :: Bool
+    }
+
+argumentParser :: O.Parser Arguments
+argumentParser = Arguments
+    <$> O.optional (O.strOption
+        ( O.long "script"
+        <> O.metavar "FILE"
+        <> O.help "Script file to execute" ))
+    <*> O.switch
+        ( O.long "show-ast"
+        <> O.help "Only show AST without executing" )
--- src/ToyLisp.hs
+++ src/ToyLisp.hs
@@ -1,1 +1,28 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module ToyLisp where
+
+import           ToyLisp.Evaluator (EvalIO (..), eval)
+import           ToyLisp.Parser    (parse)
+
+data RunConfig = RunConfig
+    { runMode     :: RunMode
+    , runPreloads :: [String]
+    }
+
+data RunMode = ExecuteProgram | ShowAstOnly
+
+runWith :: (EvalIO m) => RunConfig -> String -> m ()
+runWith config content = do
+    case parse content of
+        Left err -> writeErrorLn $ show err
+        Right ast -> do
+            case config.runMode of
+                ExecuteProgram -> do
+                    !_ <- eval ast
+                    pure ()
+                ShowAstOnly -> do
+                    writeOutputLn $ show ast
+
+runReplWith :: (EvalIO m) => RunConfig -> m ()
+runReplWith _config = error "REPL mode not implemented"
