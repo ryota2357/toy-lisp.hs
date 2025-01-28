c20fee642c533861d5a0ff7766c7259d6e3e9a9e
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-01-28 22:53:01 +0900

    feat: install shell completion with Nix

--- flake.nix
+++ flake.nix
@@ -22,9 +22,26 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         hPkgs = pkgs.haskell.packages."ghc984"; # Stackage LTS 23.5
+        lib = pkgs.lib;
+        stdenv = pkgs.stdenv;
       in
       {
-        packages.default = hPkgs.callCabal2nix "toy-lisp" ./. { };
+        packages.default = (hPkgs.callCabal2nix "toy-lisp" ./. { }).overrideAttrs (old: {
+          nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.installShellFiles ];
+          postInstall = ''
+            if ${lib.boolToString (stdenv.buildPlatform.canExecute stdenv.hostPlatform)}; then
+              TOY_LISP="$out/bin/toy-lisp"
+              PATCH_FISH_COMP_SCRIPT="complete -c toy-lisp -n '__fish_prev_arg_in --script' -a '(__fish_complete_path)'"
+              PATCH_ZSH_COMP_SCRIPT="_files" # How to patch zsh completion script only after --script option?
+              installShellCompletion --cmd toy-lisp \
+                --bash <("$TOY_LISP" --bash-completion-script "$TOY_LISP") \
+                --fish <("$TOY_LISP" --fish-completion-script "$TOY_LISP"; echo "$PATCH_FISH_COMP_SCRIPT") \
+                --zsh  <("$TOY_LISP" --zsh-completion-script  "$TOY_LISP"; echo "$PATCH_ZSH_COMP_SCRIPT")
+            else
+              echo "Skipping shell completion installation due to cross-compilation."
+            fi
+          '';
+        });
 
         devShells.default = pkgs.mkShellNoCC {
           packages = [
