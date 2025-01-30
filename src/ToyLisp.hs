62ad29b7ca22eb4edd755253bbb68f8f27b8458b
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-01-30 12:54:23 +0900

    feat: implement repl

--- src/ToyLisp.hs
+++ src/ToyLisp.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 
-module ToyLisp where
+module ToyLisp (RunConfig (..), RunMode(..), runWith, runReplWith) where
 
 import           ToyLisp.Evaluator (eval)
 import           ToyLisp.Parser    (parse)
@@ -26,4 +26,56 @@ runWith config content = do
                     RT.writeOutputLn $ show ast
 
 runReplWith :: (RT.ExecIO m) => RunConfig -> m ()
-runReplWith _config = error "REPL mode not implemented"
+runReplWith config = do
+    RT.writeOutputLn "ToyLisp REPL"
+    case config.runMode of
+        ExecuteProgram -> executeProgramLoop
+        ShowAstOnly    -> showAstOnlyLoop
+  where
+    executeProgramLoop = loop RT.emptyEnvironment
+      where
+        loop env = do
+            RT.writeOutput "* "
+            input <- readSExpr
+            case parse input of
+                Left err -> do
+                    RT.writeErrorLn $ show err
+                    loop env
+                Right ast -> do
+                    (result, nextEnv) <- eval env ast
+                    case result of
+                        Left err  -> RT.writeErrorLn $ show err
+                        Right obj -> do
+                            let display = RT.displayLispObjectWith (const Nothing)
+                            RT.writeOutputLn $ "\n" ++ display obj
+                    loop nextEnv
+    showAstOnlyLoop = do
+        input <- readSExpr
+        case parse input of
+            Left err  -> RT.writeErrorLn $ show err
+            Right ast -> RT.writeOutputLn $ show ast
+        showAstOnlyLoop
+
+data ReadSExprState = ReadSExprState
+    { parensDepth    :: Int
+    , isInsideString :: Bool
+    }
+
+readSExpr :: (RT.ExecIO m) => m String
+readSExpr = readLoop "" ReadSExprState { parensDepth = 0, isInsideString = False }
+  where
+    readLoop currentString state = do
+        inputedLine <- (++ "\n") <$> RT.readInputLine
+        let nextString = currentString ++ inputedLine
+        let nextState = updateState state inputedLine
+        if (nextState.parensDepth < 0) || (nextState.parensDepth == 0 && not nextState.isInsideString)
+            then return nextString
+            else readLoop nextString nextState
+    updateState state line = foldl updateByChar state $ zip (Nothing : map Just line) line
+      where
+        updateByChar s (prev, cur) = case cur of
+            '(' | not s.isInsideString -> s { parensDepth = s.parensDepth + 1 }
+            ')' | not s.isInsideString -> s { parensDepth = s.parensDepth - 1 }
+            '"' | not s.isInsideString -> s { isInsideString = True }
+            '"' | prev /= Just '\\'    -> s { isInsideString = False }
+            _                          -> s
