15990b7f785dabaaa6761ca470942418fabb6097
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2025-02-02 15:08:53 +0900

    feat: impl builtin functions (=,/=,<,<=,>,>=,car,cdr,list,nthcdr,prog1,progn)

--- src/ToyLisp/Evaluator.hs
+++ src/ToyLisp/Evaluator.hs
@@ -10,12 +10,13 @@ import           Control.Monad.Except (ExceptT, MonadError, runExceptT,
 import           Control.Monad.State  (StateT, get, gets, lift, modify', put,
                                        runStateT)
 import           Data.Function        (fix)
+import           Data.List            (uncons)
 import qualified Data.Map.Strict      as M
 import qualified Data.Text            as T
 import qualified ToyLisp.Runtime      as RT
 import           ToyLisp.Runtime      (Environment (..), ExecIO,
-                                       FunctionInfo (..), LispObject (..),
-                                       RuntimeError (..))
+                                       FunctionInfo (..), LexicalFrame (..),
+                                       LispObject (..), RuntimeError (..))
 import           ToyLisp.Syntax       (Ast (..), AstNode (..), Symbol,
                                        TextRange, astNodePosition, unSymbol)
 
@@ -100,6 +101,7 @@ callFunction fn args =
             case result of
                 Left err -> throwError err
                 Right obj  -> do
+                    -- Restore the environment with the updated global bindings
                     put env { globalBindings = nextEnv'.globalBindings }
                     pure $ Right obj
 
@@ -111,7 +113,7 @@ systemValueBindingsMap = M.fromList
     ]
 
 systemFunctionBindingsMap :: forall m. (ExecIO m) => M.Map Symbol ([AstNode] -> Evaluator m (Either T.Text LispObject))
-systemFunctionBindingsMap = M.fromList
+systemFunctionBindingsMap = M.fromList (
     [ ("+", \args -> do
         argValues <- mapM evalNode args
         pure $ foldM (\acc v -> case (acc, v) of
@@ -173,7 +175,77 @@ systemFunctionBindingsMap = M.fromList
                 _                          -> Left "Arguments are not numbers"
                 ) e es
       )
-    , ("defun", \args -> case args of
+    ]
+    ++
+    map (\(op, opI, opF) -> (op, \args -> do
+        argValues <- mapM evalNode args
+        let nil = Right $ LispList []
+        let t = Right LispTrue
+        let notNumber = Left "Argument is not a number"
+        pure $ case argValues of
+            [] -> Left $ mkInvalidArgCountErrorText 0 ">= 1"
+            [LispInt _] -> t
+            [LispFloat _] -> t
+            [_] -> notNumber
+            LispInt x : xs -> case () of
+                _ | Right xs' <- forM xs (\case LispInt i -> Right i; _ -> Left ()) ->
+                    if all (`opI` x) xs' then t else nil
+                  | Right xs' <- forM xs (\case LispInt i -> Right $ fromIntegral i; LispFloat f -> Right f; _ -> Left ()) ->
+                    if all (`opF` fromIntegral x) xs' then t else nil
+                  | otherwise -> notNumber
+            LispFloat x : xs -> case () of
+                _ | Right xs' <- forM xs (\case LispInt i -> Right $ fromIntegral i; LispFloat f -> Right f; _ -> Left ()) ->
+                    if all (`opF` x) xs' then t else nil
+                  | otherwise -> notNumber
+            _ -> notNumber
+        ))
+    [ ("=",  (==), (==))
+    , ("/=", (/=), (/=))
+    ]
+    ++
+    map (\(op, opI, opF) -> (op, \args -> do
+        argValues <- mapM evalNode args
+        let notNumber = Left "Argument is not a number"
+        pure $ case argValues of
+            [] -> Left $ mkInvalidArgCountErrorText 0 ">= 1"
+            [LispInt _] -> Right LispTrue
+            [LispFloat _] -> Right LispTrue
+            [_] -> notNumber
+            _ : xs -> case foldM (\_ v -> case v of
+                    (LispInt a, LispInt b)     -> if a `opI` b then Right () else Left True
+                    (LispFloat a, LispFloat b) -> if a `opF` b then Right () else Left True
+                    (LispInt a, LispFloat b)   -> if fromIntegral a `opF` b then Right () else Left True
+                    (LispFloat a, LispInt b)   -> if a `opF` fromIntegral b then Right () else Left True
+                    _ -> Left False
+                    ) () $ zip argValues xs
+                of
+                    Right _    -> Right LispTrue
+                    Left True  -> Right $ LispList []
+                    Left False -> notNumber
+        ))
+    [ (">", (>), (>))
+    , ("<", (<), (<))
+    , (">=", (>=), (>=))
+    , ("<=", (<=), (<=))
+    ]
+    ++
+    [ ("car", \args -> do
+        argValues <- mapM evalNode args
+        pure $ case argValues of
+            [LispList []]      -> Right $ LispList []
+            [LispList (x : _)] -> Right x
+            [_]                -> Left "Argument is not a list"
+            _                  -> Left $ mkInvalidArgCountErrorText (length args) "1"
+      )
+    , ("cdr", \args -> do
+        argValues <- mapM evalNode args
+        pure $ case argValues of
+            [LispList []]       -> Right $ LispList []
+            [LispList (_ : xs)] -> Right $ LispList xs
+            [_]                -> Left "Argument is not a list"
+            _                  -> Left $ mkInvalidArgCountErrorText (length args) "1"
+      )
+    , ("defun", \case
         SymbolNode _ fnName : ListNode _ params : body -> if fnName `M.member` (systemFunctionBindingsMap @m)
             then pure $ Left $ "Redefining " <> unSymbol fnName <> " is not allowed"
             else case forM params (\case SymbolNode _ s -> Right s; _ -> Left ()) of
@@ -188,7 +260,66 @@ systemFunctionBindingsMap = M.fromList
         SymbolNode _ _ : invalidParam | not $ null invalidParam ->
             pure $ Left "Function parameters are not a list"
         _ : _ : _ -> pure $ Left "Function name is not a symbol"
-        _ -> pure $ Left $ mkInvalidArgCountErrorText (length args) ">= 2"
+        args -> pure $ Left $ mkInvalidArgCountErrorText (length args) ">= 2"
+      )
+    , ("if", \case
+        [cond, thenExpr] -> do
+            condValue <- evalNode cond
+            case condValue of
+                LispList [] -> pure $ Right $ LispList []
+                _           -> Right <$> evalNode thenExpr
+        [cond, thenExpr, elseExpr] -> do
+            condValue <- evalNode cond
+            Right <$> case condValue of
+                LispList [] -> evalNode elseExpr
+                _           -> evalNode thenExpr
+        args -> pure $ Left $ mkInvalidArgCountErrorText (length args) "2 or 3"
+      )
+    , ("let", \case
+        ListNode _ bindings : body -> do
+            case forM bindings (\case
+                    ListNode _ (SymbolNode _ sym : values) -> case uncons values of
+                        Just (value, []) -> Right (sym, Just value)
+                        Just (_, _) ->  Left $ "Binding for " <> unSymbol sym <> " is not a single value"
+                        Nothing -> Right (sym, Nothing)
+                    SymbolNode _ sym -> Right (sym, Nothing)
+                    _ -> Left "Variable name is not a symbol")
+                of
+                Left err -> pure $ Left err
+                Right bindings' -> do
+                    nextBindingMap <- M.fromList <$> forM bindings' (
+                        \(sym, value) -> (sym,) <$> case value of
+                            Just value' -> evalNode value'
+                            Nothing     -> pure $ LispList [])
+                    env <- get
+                    let nextEnv = env
+                            { currentLexicalFrame = LexicalFrame
+                                { lexicalValueBindings = nextBindingMap
+                                , lexicalFunctionBindings = M.empty
+                                , parentLexicalFrame = Just env.currentLexicalFrame
+                                }
+                            }
+                    (result, nextEnv') <- lift . lift $ eval nextEnv (Ast body)
+                    case result of
+                        Left err -> throwError err
+                        Right obj -> do
+                            -- Restore the environment with the updated global bindings
+                            put env { globalBindings = nextEnv'.globalBindings }
+                            pure $ Right obj
+        _ : _ -> pure $ Left "Bindings are not a list"
+        args -> pure $ Left $ mkInvalidArgCountErrorText (length args) ">= 1"
+      )
+    , ("list", \args -> do
+        argValues <- mapM evalNode args
+        pure $ Right $ LispList argValues
+      )
+    , ("nthcdr", \args -> do
+        argValues <- mapM evalNode args
+        pure $ case argValues of
+            [LispInt n, LispList xs] -> Right $ LispList $ drop (fromIntegral n) xs
+            [LispInt _, _]           -> Left "Second argument is not a list"
+            [_, _]                   -> Left "First argument is not an integer"
+            _                        -> Left $ mkInvalidArgCountErrorText (length args) "2"
       )
     , ("princ", \args -> do
         argValues <- mapM evalNode args
@@ -201,6 +332,18 @@ systemFunctionBindingsMap = M.fromList
                 pure $ Right arg
             _ -> pure $ Left $ mkInvalidArgCountErrorText (length args) "1"
       )
+    , ("prog1", \args -> do
+        argValues <- mapM evalNode args
+        pure $ case uncons argValues of
+            Just (first, _) -> Right first
+            Nothing         -> Right $ LispList []
+      )
+    , ("progn", \args -> do
+        argValues <- mapM evalNode args
+        pure $ case argValues of
+            [] -> Right $ LispList []
+            _  -> Right $ last argValues
+      )
     , ("quote", \args -> pure $ case args of
         [arg] -> Right $ fix (\quote -> \case
             IntNode _ i -> LispInt i
@@ -211,7 +354,7 @@ systemFunctionBindingsMap = M.fromList
             ) arg
         _ -> Left $ mkInvalidArgCountErrorText (length args) "1"
       )
-    , ("setq", \args -> case args of
+    , ("setq", \case
         [SymbolNode _ sym, value] -> if sym `M.member` systemValueBindingsMap
             then pure $ Left $ unSymbol sym <> " is a constant and thus cannot be set"
             else do
@@ -221,7 +364,7 @@ systemFunctionBindingsMap = M.fromList
                     }
                 pure $ Right value'
         [_, _] -> pure $ Left "Variable name is not a symbol"
-        _ -> pure $ Left $ mkInvalidArgCountErrorText (length args) "2"
+        args -> pure $ Left $ mkInvalidArgCountErrorText (length args) "2"
       )
     , ("type-of", \args -> do
         argValues <- mapM evalNode args
@@ -236,7 +379,7 @@ systemFunctionBindingsMap = M.fromList
                     LispTrue       -> "T"
             _ -> Left $ mkInvalidArgCountErrorText (length args) "1"
       )
-    ]
+    ])
   where
     mkInvalidArgCountErrorText :: Int -> String -> T.Text
     mkInvalidArgCountErrorText given expected = "Wrong number of arguments: given " <> givenText <> ", expected " <> expectedText
